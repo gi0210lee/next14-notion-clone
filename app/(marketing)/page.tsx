3aeb11d5b8291c226c06838120146bec270e8e09
gi0210lee <gi0210lee@gmail.com>
2024-10-14 11:37:48 +0900

    fix

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,8 +5,8 @@ import Footer from "./_components/footer";
 
 const MarketingPage = () => {
   return (
-    <div className="min-h-full flex flex-col">
-      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-6 pb-10 flex-1 ">
+    <div className="flex min-h-full flex-col dark:bg-[#1f1f1f]">
+      <div className="flex flex-1 flex-col items-center justify-center gap-y-6 pb-10 text-center md:justify-start">
         <Heading />
         <Heroes />
       </div>
